0c3cdcde6566de5991da547b1163d0e015d47549
sanjana oza <120481545+Sanjana-2309@users.noreply.github.com>
2026-04-01 17:42:13 +0530

    Delete BookMyTurfwebservices/Models directory

--- BookMyTurfwebservices/Models/Settings/ApiSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookMyTurfwebservices.Models.Settings;
-
-public class ApiSettings
-{
-    public string ApiKey { get; set; } = string.Empty;
-    public string[] AllowedOrigins { get; set; } = Array.Empty<string>();
-    public int RateLimitRequests { get; set; } = 100;
-    public int RateLimitPeriodMinutes { get; set; } = 1;
-}
--- BookMyTurfwebservices/Models/Settings/SlotLockSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookMyTurfwebservices.Models.Settings;
-
-public class SlotLockSettings
-{
-    public int LockDurationMinutes { get; set; } = 10;
-    public int CleanupIntervalMinutes { get; set; } = 5;
-    public int MaxLockExtensions { get; set; } = 2;
-}
